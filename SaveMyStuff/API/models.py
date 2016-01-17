05969d9e7e77ea7d9b4c5129d408080cd10d0d72
Jacksonbaker323 <jacksonbaker323@Gmail.com>
2016-01-16 22:43:40 -0800

    Added Token authentication

--- SaveMyStuff/API/models.py
+++ SaveMyStuff/API/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+#Setup for Token Auth
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+#End setup for Token Auth
+
 class Category(models.Model):
     #Name of the Category
     category_name = models.CharField(max_length=200)
--- SaveMyStuff/SaveMyStuff/urls.py
+++ SaveMyStuff/SaveMyStuff/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from API import views
+from rest_framework.authtoken import views as authviews
 
 
 
@@ -15,6 +16,7 @@ router.register(r'propertyitems', views.PropertyItemViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', authviews.obtain_auth_token)
 ]
 
